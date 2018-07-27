8fadbb8a1da884436be7ada965ee292fcbf3d364
UrielLoreto <uriel.loreto8@gmail.com>
2018-07-26 23:25:01 -0500

    Uriel
    Modificacion de modelos

--- ProyectSecusoft/cita/models.py
+++ ProyectSecusoft/cita/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class CitaEstatus(models.Model):
-    id_estatus = models.AutoField(primary_key=True)
-    estatus = models.CharField(max_length=20)
-    descripcion = models.CharField(max_length=50)
-
 class Cita(models.Model):
-    id_cita = models.AutoField(max_length= 20, primary_key=True)
+    estatus_tipo = (
+        ('1', 'Solicitada'),
+        ('2', 'Aceptada'),
+        ('3', 'Atendida'),
+    )
+    id_cita = models.AutoField(max_length=20, primary_key=True)
     fecha_cita = models.DateTimeField()
     asunto = models.CharField(max_length=200)
     observaciones = models.CharField(max_length=500)
-    estatus = models.ForeignKey(CitaEstatus, on_delete=models.CASCADE)
+    estatus = models.CharField(max_length=2, choices=estatus_tipo, default='1')
+    descripcion = models.CharField(max_length=50)
--- ProyectSecusoft/incidencia/models.py
+++ ProyectSecusoft/incidencia/models.py
@@ -1,11 +1,8 @@
 from django.db import models
+from usuario.models import Docente, Padre_Familia, Alumno
+from cita.models import Cita
 
 # Create your models here.
-
-class EstatusIncidencia(models.Model):
-    id_estatus = models.AutoField(primary_key=True)
-    estatus = models.CharField(max_length=20)
-    descripcion = models.CharField(max_length=100)
 
 class TipoIndicencia(models.Model):
     id_tipo = models.AutoField(max_length= 10, primary_key=True)
@@ -13,10 +10,48 @@ class TipoIndicencia(models.Model):
     descripcion = models.CharField(max_length=50)
     impacto = models.CharField(max_length= 10)
 
+
 class Incidencia(models.Model):
-    id_incidencia = models.AutoField(max_length= 20, primary_key=True)
+    estatus_tipo = (
+        ('1', 'Reportada'),
+        ('2', 'En Proceso'),
+        ('3', 'Atendida'),
+        ('4', 'Otro'),
+    )
+    id_incidencia = models.AutoField(max_length=20, primary_key=True)
     fecha_incidencia = models.DateTimeField()
     asunto = models.CharField(max_length=200)
-    estatus = models.ForeignKey(EstatusIncidencia, on_delete=models.CASCADE)
+    estatus = models.CharField(max_length=2, choices=estatus_tipo, default='1')
     observaciones = models.CharField(max_length=500)
-    tipo = models.ForeignKey(TipoIndicencia, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(TipoIndicencia, null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Incidencia_docente(models.Model):
+    incidencia = models.ManyToManyField(Incidencia)
+    docente = models.ManyToManyField(Docente)
+
+
+class Incidencia_padre(models.Model):
+    incidencia = models.ManyToManyField(Incidencia)
+    padre = models.ManyToManyField(Padre_Familia)
+
+
+class Incidencia_alumno(models.Model):
+    incidencia = models.ManyToManyField(Incidencia)
+    alumno = models.ManyToManyField(Alumno)
+
+
+class Incidencia_cita(models.Model):
+    incidencia = models.ManyToManyField(Incidencia)
+    cita = models.ManyToManyField(Cita)
+
+
+class Comentario(models.Model):
+    id_comentario = models.AutoField(max_length=20, primary_key=True)
+    comentario = models.CharField(max_length=500, null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+class Comentario_rel(models.Model):
+    comentario = models.ManyToManyField(Comentario)
+    rel = models.ManyToManyField(Cita)
+    persona = models.ManyToManyField(Docente)
